9943c3a92ccc6a17d5e1fbd94c0cfaf3c88d3687
cmander <cmcov@live.com>
2014-10-13 15:30:37 +0100

    More completed tasks from worksheet.

---
+++ 5551460/aircraft_seats.py
@@ -0,0 +1,12 @@
+seats = None
+rows = None
+columns = None
+
+
+rows = input("number of rows: ")
+
+columns = 8
+
+seats = rows * columns
+
+print(str('Seats: ') + str(seats))
---
+++ 5551460/euro_converter.py
@@ -0,0 +1,12 @@
+def converter(x, y):
+    print(float(x) * float(y))
+    return
+
+Euro = 1.27
+
+while True:
+    print("Input the quantity of GBP that you wish to convert...")
+    GBP = input()
+    converter(GBP, Euro)
+    input()
+
