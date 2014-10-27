0f09988a4741f121c482052e623bcde55051aa7c
HarryMills <harry.e.mills@gmail.com>
2014-10-27 18:54:35 +0000

    Added combobox for selection of map
    Removed upload button code

--- VirtualRobot.py
+++ VirtualRobot.py
@@ -10,18 +10,20 @@ window.resizable(width=FALSE, height=FALSE)
 #Creating buttons
 btnStart=Button(window, text='Start', height=1, width=20) #Add command once coded
 btnStop=Button(window, text='Stop', height=1, width=20) #Add command once coded
-btnUpload=Button(window, text='Upload..', height=1, width=20) #Add command once coded
 btnReset=Button(window, text='Reset', height=1, width=20) #Add command once coded
 
 #Places buttons in correct positions
 btnStart.place(x=11, y=500)
 btnStop.place(x=11, y=530)
-btnUpload.place(x=712, y=500)
 btnReset.place(x=712, y=530)
 
 #Creating label and positioning
 lblTimer=Label(text='00:00:00', font=('Helvetica', 20))
 lblTimer.place(x=400, y=500)
+
+#Creating ComboBox
+cmbMap=ttk.Combobox(window, values=["Map 1", "Map 2", "Map 3", "Map 4", "Map 5", "Map 6"], height=1, width=21) #Add command once coded
+cmbMap.place(x=712, y=500)
 
 #Padds canvas
 canvas.pack(padx=10,pady=10)
